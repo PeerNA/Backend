3741beaa410aba9eb4a851316f228efca2e48ca4
sseunghoon <sseunghun99@naver.com>
2023-03-18 15:36:26 +0900

    init project

---
+++ src/main/java/cos/peerna/PeernaApplication.java
@@ -0,0 +1,13 @@
+package cos.peerna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeernaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeernaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cos/peerna/PeernaApplicationTests.java
@@ -0,0 +1,13 @@
+package cos.peerna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeernaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
