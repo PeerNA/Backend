54dded1286b8b36d0fcddac2f733027400400e6c
sseunghoon <sseunghun99@naver.com>
2023-04-12 22:01:19 +0900

    feat: add SessionConfig

--- src/main/java/cos/peerna/config/SecurityConfig.java
+++ src/main/java/cos/peerna/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package cos.peerna.config;
 
 import cos.peerna.security.*;
-import cos.peerna.config.auth.*;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
