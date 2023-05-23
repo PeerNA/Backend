443bf689da84dd7dd50e894efacad9fa68b3e680
its-sky <smc9919@naver.com>
2023-05-23 10:33:19 +0900

    [feature/auto-pr] refactor: changed targer user for liked reply notification

--- src/main/java/cos/peerna/service/ReplyService.java
+++ src/main/java/cos/peerna/service/ReplyService.java
@@ -105,7 +105,8 @@ public class ReplyService {
 
         /* 좋아요가 일정 수 이상 넘어가면 자동으로 PR 요청 메시지 */
         if (!reply.isRequested() && reply.getLikeCount() == 10) {
-            Notification notification = Notification.createNotification(user, reply, NotificationType.PULL_REQ, "답변이 10개 이상이 되어 PR 요청을 보냈습니다.");
+            User likedUser = reply.getUser();
+            Notification notification = Notification.createNotification(likedUser, reply, NotificationType.PULL_REQ, "답변이 10개 이상이 되어 PR 요청을 보냈습니다.");
             notificationRepository.save(notification);
             Reply.requestReply(reply);
         }
