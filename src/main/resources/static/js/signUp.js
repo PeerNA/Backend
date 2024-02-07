33447743cbc21423d1d9b405e8bc64598e5b8cd7
sseunghoon <sseunghun99@naver.com>
2024-02-08 03:36:45 +0900

    feat: SSR 클라이언트 (tailwind)

---
+++ src/main/resources/static/js/signUp.js
@@ -0,0 +1,21 @@
+function signUp() {
+    // make id random
+    const id = Math.random() * 10000;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const name = document.getElementById('name').value;
+
+    axios.post('/signup', {
+        id: id,
+        email: email,
+        password: password,
+        name: name
+    }).then(function (response) {
+        console.log(response);
+        alert('회원가입이 완료되었습니다.');
+        window.location.href = '/login';
+    }).catch(function (error) {
+        console.log(error);
+        alert('회원가입에 실패하였습니다.');
+    });
+}
