b7aa3c23742e898a34b7a98e83535aa208309f12
sseunghoon <sseunghun99@naver.com>
2024-02-08 03:37:19 +0900

    feat: #57 카테고리 개수 축소 (25 -> 6)

--- src/main/java/cos/peerna/domain/user/model/Category.java
+++ src/main/java/cos/peerna/domain/user/model/Category.java
@@ -3,7 +3,12 @@ package cos.peerna.domain.user.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum Category {
-    JAVA, JAVASCRIPT, PYTHON, SPRING, JPA, REACT, NODEJS, DJANGO, RDB, NOSQL, OS, SECURITY, NETWORK, DATA_STRUCTURE, ALGORITHM, COMPILER, TROUBLE, DESIGN, TEST, INFRA, CONTAINER, DEVOPS, COMMUNICATION, WORK, TECH, CODING_TEST;
+    DATA_STRUCTURE_ALGORITHM,
+    OPERATING_SYSTEM,
+    NETWORK,
+    DATABASE,
+    SECURITY,
+    PERSONALITY;
 
     @JsonCreator
     public static Category from(String s) {
