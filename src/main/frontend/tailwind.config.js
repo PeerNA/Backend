b1b21f2260eaf981b7bb48c7a59520d3a6ebd451
sseunghoon <sseunghun99@naver.com>
2024-03-04 20:27:42 +0900

    feat: home, study-solo 페이지 Thymeleaf + tailwind.css (레이아웃 구성)
    defaultLayout 속 config.html, header.html, footer.html 등
    
    signUp.html 과
    login.html 은 테스트를 위한 페이지로 서비스에선 제거하거나 어드민 페이지로 옮겨야 함

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["../resources/templates/**/*.{html,js}",
+        "../resources/templates/**/**/*.{html,js}",
         "./node_modules/flowbite/**/*.js"], // it will be explained later
     theme: {
         extend: {},
