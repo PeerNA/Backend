4014243cd3827938ba46a3b52509656c647ada6b
sseunghoon <sseunghun99@naver.com>
2024-03-04 22:40:48 +0900

    test: ProblemRegisterRequestDto -> RegisterProblemRequest 변경 및 테스트 보완

--- src/test/java/cos/peerna/service/ProblemServiceTest.java
+++ src/test/java/cos/peerna/service/ProblemServiceTest.java
@@ -1,6 +1,8 @@
 package cos.peerna.service;
 
-import cos.peerna.domain.problem.dto.ProblemRegisterRequestDto;
+import static org.junit.Assert.assertThrows;
+
+import cos.peerna.domain.problem.dto.request.RegisterProblemRequest;
 import cos.peerna.domain.problem.service.ProblemService;
 import cos.peerna.domain.user.model.Category;
 import cos.peerna.domain.problem.model.Problem;
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @SpringBootTest
 @Transactional
@@ -22,11 +25,22 @@ class ProblemServiceTest {
 	@Test
 	@DisplayName("중복 문제 검증")
 	void make() {
-		ProblemRegisterRequestDto problemRegisterRequestDto = new ProblemRegisterRequestDto();
-		problemRegisterRequestDto.setCategory(Category.JAVA);
-		problemRegisterRequestDto.setQuestion("hello");
-		problemRegisterRequestDto.setAnswer("안녕");
-		Problem problem = Problem.createProblem(problemRegisterRequestDto);
+		// 문제 등록
+		RegisterProblemRequest request = RegisterProblemRequest.of(
+				"hello", "안녕", Category.OPERATING_SYSTEM);
+		Problem problem = Problem.createProblem(
+				request.question(), request.answer(), request.category()
+		);
 		problemRepository.save(problem);
+
+		// 동일한 내용의 문제 등록 시도
+		RegisterProblemRequest duplicateRequest = RegisterProblemRequest.of(
+				"hello", "안녕", Category.OPERATING_SYSTEM);
+		Problem duplicateProblem = Problem.createProblem(
+				duplicateRequest.question(), duplicateRequest.answer(), duplicateRequest.category()
+		);
+
+		// 동일한 내용의 문제를 등록하려고 시도하면 예외가 발생해야 함
+		assertThrows(ResponseStatusException.class, () -> problemRepository.save(duplicateProblem));
 	}
 }
