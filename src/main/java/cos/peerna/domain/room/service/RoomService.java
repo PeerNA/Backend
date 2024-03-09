e85a9883e4619be5a3ffa4f1614c4a45c70c7ff7
sseunghoon <sseunghun99@naver.com>
2024-03-10 02:31:48 +0900

    feat: '사람과 함께 학습' 채팅 저장

--- src/main/java/cos/peerna/domain/room/service/RoomService.java
+++ src/main/java/cos/peerna/domain/room/service/RoomService.java
@@ -1,9 +1,15 @@
 package cos.peerna.domain.room.service;
 
+import com.amazonaws.services.kms.model.NotFoundException;
 import cos.peerna.domain.history.model.History;
 import cos.peerna.domain.history.repository.HistoryRepository;
+import cos.peerna.domain.problem.model.Problem;
+import cos.peerna.domain.problem.repository.ProblemRepository;
+import cos.peerna.domain.room.dto.response.ChangeProblemResponse;
 import cos.peerna.domain.room.event.CreateRoomEvent;
+import cos.peerna.domain.room.model.Chat;
 import cos.peerna.domain.room.model.Room;
+import cos.peerna.domain.room.repository.ChatRepository;
 import cos.peerna.domain.room.repository.RoomRepository;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -23,6 +29,8 @@ public class RoomService {
     private final SimpMessagingTemplate template;
     private final RoomRepository roomRepository;
     private final HistoryRepository historyRepository;
+    private final ChatRepository chatRepository;
+    private final ProblemRepository problemRepository;
 
     public void createRoom(CreateRoomEvent event) {
         List<Long> connectedUserIds = new ArrayList<>(event.userScore().keySet());
@@ -41,7 +49,49 @@ public class RoomService {
                         .build());
 
         for (Long userId : connectedUserIds) {
-            template.convertAndSend("/user/" + userId + "/match/join", room);
+            template.convertAndSend("/user/" + userId + "/match", room.getId());
         }
+    }
+
+    public boolean isConnectedUser(Integer roomId, Long userId) {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new IllegalArgumentException("Room not found"));
+        return room.isConnectedUser(userId);
+    }
+
+    public Room findById(Integer roomId) {
+        return roomRepository.findById(roomId)
+                .orElseThrow(() -> new NotFoundException("Room not found"));
+    }
+
+    /*
+    TODO: 학습이 끝날 때까지 채팅 캐시 후 한번에 저장
+     */
+    public void saveChat(Integer roomId, Long userId, String message) {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new NotFoundException("Room not found"));
+        History history = historyRepository.findById(room.getLastHistoryId())
+                .orElseThrow(() -> new NotFoundException("History not found"));
+
+        chatRepository.save(Chat.builder()
+                .writerId(userId)
+                .content(message)
+                .history(history)
+                .build());
+    }
+
+    public ChangeProblemResponse changeProblem(Integer roomId, String userName, Long problemId) {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new NotFoundException("Room not found"));
+        History history = historyRepository.findById(room.getLastHistoryId())
+                .orElseThrow(() -> new NotFoundException("History not found"));
+        Problem problem = problemRepository.findById(problemId)
+                .orElseThrow(() -> new NotFoundException("Problem not found"));
+
+        history.updateProblem(problem);
+        historyRepository.save(history);
+
+        return ChangeProblemResponse.of(problem.getId(), problem.getQuestion(),
+                String.format("%s님이 문제를 변경했습니다.", userName));
     }
 }
