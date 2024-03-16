4483f0c3d9927fea01544091036fc2a7a65064ed
sseunghoon <sseunghun99@naver.com>
2024-03-16 22:16:51 +0900

    build: remote setting

--- deploy-debug.sh
+++ deploy-debug.sh
@@ -5,10 +5,10 @@
 
 # Docker 이미지 빌드 및 푸시
 DOCKER_USERNAME=seunghso
-DOCKER_IMAGE_NAME=peerna
+DOCKER_IMAGE_NAME=peerna-debug
 
 # Linux AMD64 용 이미지 빌드
-docker build --build-arg DEPENDENCY=build/dependency -t $DOCKER_USERNAME/$DOCKER_IMAGE_NAME --platform linux/amd64 .
+docker build -f Dockerfile-debug --build-arg DEPENDENCY=build/dependency -t $DOCKER_USERNAME/$DOCKER_IMAGE_NAME --platform linux/amd64 .
 
 # 이미지 푸시
 docker push $DOCKER_USERNAME/$DOCKER_IMAGE_NAME
