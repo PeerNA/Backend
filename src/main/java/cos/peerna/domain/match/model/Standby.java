e80355577ff7e468c4dc111c4ba9f6be4cd747b6
sseunghoon <sseunghun99@naver.com>
2024-03-20 18:12:59 +0900

    feat: Standby 구현체 hashes 로 변경

--- src/main/java/cos/peerna/domain/match/model/Standby.java
+++ src/main/java/cos/peerna/domain/match/model/Standby.java
@@ -1,20 +1,35 @@
 package cos.peerna.domain.match.model;
 
+import cos.peerna.domain.user.model.Category;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
-public class Standby {
-
+@RedisHash("Peerna:Match:Standby")
+public final class Standby {
+    @Id
     private final Long id;
+
+    @Indexed
+    @Enumerated(EnumType.STRING)
+    private final Category category;
+
     private final Integer score;
+
     private final LocalDateTime createdAt;
 
+
     @Builder
-    public Standby(Long id, Integer score, LocalDateTime createdAt) {
+    public Standby(Long id, Category category, Integer score, LocalDateTime createdAt) {
         this.id = id;
+        this.category = category;
         this.score = score;
         this.createdAt = createdAt;
     }
@@ -23,5 +38,13 @@ public class Standby {
         long waitingTime = ChronoUnit.SECONDS.between(this.createdAt, LocalDateTime.now());
         int scoreGap = Math.abs(this.score - standby.score);
         return scoreGap <= 50 + Math.log(1 + waitingTime) / Math.log(2);
+    }
+
+    @Override
+    public String toString() {
+        return "Standby[" +
+                "id=" + id + ", " +
+                "score=" + score + ", " +
+                "createdAt=" + createdAt + ']';
     }
 }
