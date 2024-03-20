7f27dde5f98de0991a93a9dd0660fefe15446798
sseunghoon <sseunghun99@naver.com>
2024-03-20 18:08:20 +0900

    refac: MatchService 에 존재하던 MatchJob의 역할 이전

--- src/main/java/cos/peerna/domain/match/job/MatchJob.java
+++ src/main/java/cos/peerna/domain/match/job/MatchJob.java
@@ -1,12 +1,17 @@
 package cos.peerna.domain.match.job;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import static org.quartz.JobBuilder.newJob;
+
 import cos.peerna.domain.match.service.MatchService;
 import cos.peerna.domain.user.model.Category;
 import lombok.RequiredArgsConstructor;
+import org.quartz.CronScheduleBuilder;
 import org.quartz.InterruptableJob;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
 import org.quartz.UnableToInterruptJobException;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.quartz.QuartzJobBean;
@@ -15,6 +20,11 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class MatchJob extends QuartzJobBean implements InterruptableJob {
+    private static final String DEVELOPER_NAME = "송승훈";
+    private static final String JOB_DESCRIPTION = "동료 매칭";
+    private static final String SCHEDULE_EXPRESSION = "*/5 * * * * ?";
+    private static final String JOB_IDENTITY = "Dev";
+    private static final String JOB_WORK = "Work";
 
     private final RedisTemplate<String, String> redisTemplate;
     private final MatchService matchService;
@@ -24,13 +34,27 @@ public class MatchJob extends QuartzJobBean implements InterruptableJob {
     }
 
     @Override
-    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+    protected void executeInternal(JobExecutionContext context) {
         for (Category category : Category.values()) {
-            try {
-                matchService.duoMatching(category);
-            } catch (JsonProcessingException e) {
-                redisTemplate.delete("standby:" + category.name());
-            }
+            matchService.duoMatching(category);
         }
+    }
+
+    public static Trigger buildJobTrigger() {
+        return TriggerBuilder.newTrigger()
+                .withSchedule(CronScheduleBuilder.cronSchedule(SCHEDULE_EXPRESSION)).build();
+    }
+
+    public static JobDetail buildJobDetail() {
+        return newJob(MatchJob.class).withIdentity("scheduleJob")
+                .usingJobData(newJobDataMap(DEVELOPER_NAME, JOB_DESCRIPTION))
+                .build();
+    }
+
+    private static JobDataMap newJobDataMap(String name, String work) {
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put(JOB_IDENTITY, name);
+        jobDataMap.put(JOB_WORK, work);
+        return jobDataMap;
     }
 }
